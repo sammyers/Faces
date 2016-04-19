3eecd5fc4233751d1808ab912a6ea30551fe446a
Danny Wolf <2dawolf@gmail.com>
2016-04-19 13:49:43 -0400

    Make it work.

--- images.py
+++ images.py
@@ -4,7 +4,7 @@ import os
 import glob
 
 import numpy as np
-import scipy
+import scipy.misc
 
 class ImageDatabase(object):
     """
@@ -12,51 +12,52 @@ class ImageDatabase(object):
 
     Provides helper functionality for dealing with a folder of images
     """
-    image_filetype = "png"
-    use_grayscale = True
-    image_caching = True
-
-    split_char = "_"
-
-    name_idx = 1
-    magnitude_idx = 2
 
 
     def __init__(self, directory='face_images'):
         super(ImageDatabase, self).__init__()
 
-        if not _check_valid_directory(directory):
+        self.image_filetype = "png"
+        self.use_grayscale = True
+        self.image_caching = True
+
+        self.split_char = "_"
+
+        self.name_idx = 1
+        self.magnitude_idx = 2
+
+        if not self._check_valid_directory(directory):
             raise Exception("Invalid directory")
 
         self.directory = directory
 
-        self.image_list = sorted(_get_image_list(self.directory))
+        self.image_list = sorted(self._get_image_list(self.directory))
 
         self.cached_images = {}
 
 
-    def _get_image_list(directory):
+    def _get_image_list(self, directory):
         """
         Get all images of the right type in directory
         """
-        glob_path = os.path.join(directory, "*.{}".format(image_filetype))
-        return glob.glob(glob_path)
+        glob_path = os.path.join(directory, "*.{}".format(self.image_filetype))
+        return [os.path.basename(f_name) for f_name in glob.glob(glob_path)]
 
 
-    def _images_in_directory(directory):
+    def _images_in_directory(self, directory):
         # are there more than 0 images of the right type in this directory?
-        return len(_get_image_list(directory)) > 0
+        return len(self._get_image_list(directory)) > 0
 
 
-    def _check_valid_directory(directory):
+    def _check_valid_directory(self, directory):
         return (os.path.isdir(directory) and
-            _images_in_directory(directory))
+            self._images_in_directory(directory))
 
 
     def _load_image(self, name):
-        return scipy.ndimage.imread(
+        return scipy.misc.imread(
             os.path.join(self.directory, name),
-            use_grayscale
+            self.use_grayscale
         )
 
 
@@ -65,7 +66,7 @@ class ImageDatabase(object):
 
 
     def __getitem__(self, key):
-        if image_caching:
+        if self.image_caching:
             # load from cache or cache and return
             if hasattr(self.cached_images, key):
                 return self.cached_images[key]
@@ -83,7 +84,7 @@ class ImageDatabase(object):
 
 
     def _get_split_filenames(self):
-        return [f_name.split(split_char) for f_name in self.image_list]
+        return [f_name.split(self.split_char) for f_name in self.image_list]
 
 
     def _get_people_set(self):
@@ -95,18 +96,21 @@ class ImageDatabase(object):
         """
         split_f_names = self._get_split_filenames()
 
-        return set(split_f_names[:, name_idx])
+        return set([split_f_name[self.name_idx] for split_f_name in split_f_names])
+
 
     def _iterate_people_helper(self):
         """
         Yields the list of split filenames for each person in the image database,
         for use in the iterate_people method and iterate_emotions method
         """
-        people = _get_people_set()
+        split_f_names = self._get_split_filenames()
+
+        people = self._get_people_set()
 
         for person in people:
             images_of_person = filter(
-                lambda split_f_name: split_f_name[name_idx] is person,
+                lambda split_f_name: split_f_name[self.name_idx] == person,
                 split_f_names
             )
 
@@ -116,16 +120,36 @@ class ImageDatabase(object):
         for images_of_person in self._iterate_people_helper():
 
             # rejoin split string and yield array of images of the person
-            yield [self[f_name.join(split_char)] for f_name in images_of_person]
+            yield [self[self.split_char.join(split_f_name)] for split_f_name in images_of_person]
 
 
     def iterate_emotions(self):
         def make_matrix():
             for split_f_name_list in self._iterate_people_helper():
-                yield [split_f_name.join(split_char) for split_f_name in split_f_name_list]
+                yield [self.split_char.join(split_f_name) for split_f_name in split_f_name_list]
 
         # [names x emotions]
         filename_matrix = np.matrix([row for row in make_matrix()])
 
         for emotion in filename_matrix.T:
-            yield [self[img] for img in emotion]
+            yield [self[img] for img in np.array(emotion)[0]]
+
+
+if __name__ == '__main__':
+    imdb = ImageDatabase(directory="/media/wolf/Shared/Dropbox/2016/QEA/2-Faces/faces")
+
+    import matplotlib.pyplot as plt
+
+    for emotion in imdb.iterate_emotions():
+        for img in emotion:
+            plt.imshow(img, cmap=plt.cm.gray)
+            plt.show()
+            break
+        break
+
+    for person in imdb.iterate_people():
+        for emotion in person:
+            plt.imshow(img, cmap=plt.cm.gray)
+            plt.show()
+            break
+        break
